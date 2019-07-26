e94244fc91feee3e6016de9dad1eff9e780b0b87
Rana Alie <ali.ammaar@novatoresols.com>
2019-07-26 18:18:43 +0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 User.create(email: 'user@gmail.com', user_name: 'Rana Ali', phone: '03024492335', address: 'Johar Town, Lahore, Pakistan', password: 'password', password_confirmation: 'password')
 ['Dry Cleaning','Washing','Ironing','Stain Removing'].each do |name|
-	Capability.create(name: )
+	Capability.create(name: name)
 end
