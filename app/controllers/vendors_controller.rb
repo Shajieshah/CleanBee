4c759f5d4eea36c8cc1fdb63df54cda1a60180a3
zeshan9128 <zeeshan.butt@novatoresols.com>
2019-08-26 17:46:01 +0500

    fix logo height issue

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -3,6 +3,10 @@ class VendorsController < ApplicationController
 	before_action :authenticate_vendor!
 	
 	def dashboard
+		puts 'ok'
+		@new_orders = current_vendor.shop.orders.where(status: "pending").count
+		@ongoing_orders = current_vendor.shop.orders.where(status: "pending").count
+
 	end
 
 end
