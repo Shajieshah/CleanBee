0aed763092d1e2a8e75825810bb97d65a79f730d
zeshan9128 <zeeshan.butt@novatoresols.com>
2019-08-27 09:18:50 +0500

    fix auth issues

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,12 +1,13 @@
 class VendorsController < ApplicationController
 
-	before_action :authenticate_vendor!
-	
-	def dashboard
-		puts 'ok'
-		@new_orders = current_vendor.shop.orders.where(status: "pending").count
-		@ongoing_orders = current_vendor.shop.orders.where(status: "pending").count
+  before_action :authenticate_vendor!
 
-	end
+  def dashboard
+    @new_orders = current_vendor.shop.orders.where(status: "pending").count
+    @ongoing_orders = current_vendor.shop.orders.where(status: ['assigned', 'picked_up_from_customer',
+                                                                'dropped_to_vendor', 'ready_to_deliver',
+                                                                'picked_up_from_vendor']).count
+    @completed_orders = current_vendor.shop.orders.where(status: "completed").count
+  end
 
 end
